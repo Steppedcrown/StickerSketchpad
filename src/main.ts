a98a289f6defddeacc4ecf772ce2ee6be2aa924c
Steppedcrown <92701553+Steppedcrown@users.noreply.github.com>
2025-10-15 16:47:53 +0000

    Added title and canvas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const header = document.createElement("h1");
+header.textContent = "Sticker Canvas";
+document.body.appendChild(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.border = "2px solid black";
+canvas.id = "sticker-canvas";
+document.body.appendChild(canvas);
